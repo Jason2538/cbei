194e0a0301e0b6aa85ebf38af646c2740d10e384
Jason2538 <100117413+Jason2538@users.noreply.github.com>
2023-05-13 04:45:33 +0000

    ㅇ

---
+++ 5월13일 영재원.py
@@ -0,0 +1,16 @@
+m =[[int(i)  for i in input().split()] for i in range(9)]
+
+ans = m[0][0]
+
+x=0
+y=0
+
+for i in range(9):
+    for j in range(9):
+        if ans < m[i][j]:
+            ans = m[i][j]
+            x = j
+            y = i
+print(ans)
+print(y+1, x+1)
+           
